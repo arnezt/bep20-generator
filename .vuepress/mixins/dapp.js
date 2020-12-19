120ed36667e8c8d6dacc11a3ac31b68e96451e4a
vittominacori <vittoriominacori@gmail.com>
2020-12-19 18:49:26 +0100

    feat: add mainnet service receiver

--- .vuepress/mixins/dapp.js
+++ .vuepress/mixins/dapp.js
@@ -44,7 +44,7 @@ export default {
         },
       },
       serviceReceiver: {
-        mainnet: '0x1f82770FCA99FdFB422eb863Ec6CA3CACc600cFB', // todo
+        mainnet: '0x1Ce6597baB219e627Cc54Ecd8b6Bd613DDD5b4a1',
         testnet: '0x1f82770FCA99FdFB422eb863Ec6CA3CACc600cFB',
       },
       tokenList: {
